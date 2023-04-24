023cd6b29e169e01e75d198b1acdb8cd7692ae53
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-24 20:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var fame=document.getElementById("fname").value;
+    var lame=document.getElementById("lname").value;
+	
+	alert(fame+" "+lame);
 
 }
